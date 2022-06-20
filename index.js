e9cd1883159072719e2db421740e16ab3826f648
Poljak Marijan <marijan.poljak@croatiaairlines.hr>
2022-06-20 19:03:39 +0200

    Dodana interpolacija

--- index.js
+++ index.js
@@ -27,35 +27,35 @@ function createPolaznik() {
    switch (imena[random]) {
       case "Marijan":
          let polaznik1 = new Polaznik('Marijan', 'Poljak')
-         p.innerHTML = polaznik1.ime + polaznik1.prezime;
+         p.innerHTML = `${polaznik1.ime} ` + `${polaznik1.prezime}`;
          break;
       case "Valentina":
          let polaznik2 = new Polaznik('Valentina', 'Valentinić')
-         p.innerHTML = polaznik2.ime + polaznik2.prezime;
+         p.innerHTML = `${polaznik2.ime} ` + `${polaznik2.prezime}`;
          break;
       case "Ivan":
          let polaznik3 = new Polaznik('Ivan', 'Ivanić')
-         p.innerHTML = polaznik3.ime + polaznik3.prezime;
+         p.innerHTML = `${polaznik3.ime} ` + `${polaznik3.prezime}`;
          break;
       case "Antun":
          let polaznik4 = new Polaznik('Antun', 'Nakić')
-         p.innerHTML = polaznik4.ime + polaznik4.prezime;
+         p.innerHTML = `${polaznik4.ime} ` + `${polaznik4.prezime}`;
          break;
       case "Bruno":
          let polaznik5 = new Polaznik('Bruno', 'Brunić')
-         p.innerHTML = polaznik5.ime + polaznik5.prezime;
+         p.innerHTML = `${polaznik5.ime} ` + `${polaznik5.prezime}`;
          break;
       case "Nader":
-         let polaznik6 = new Polaznik('Nader', 'Naderovo prezime')
-         p.innerHTML = polaznik6.ime + polaznik6.prezime;
+         let polaznik6 = new Polaznik('Nader', 'Naderovoprezime')
+         p.innerHTML = `${polaznik6.ime} ` + `${polaznik6.prezime}`;
          break;
       case "Antonija":
-         let polaznik7 = new Polaznik('Antonija', 'Ne znamp prezime')
-         p.innerHTML = polaznik7.ime + polaznik7.prezime;
+         let polaznik7 = new Polaznik('Antonija', 'Antonijić')
+         p.innerHTML = `${polaznik7.ime} ` + `${polaznik7.prezime}`;
          break;
       case "Mladen":
-         let polaznik8 = new Polaznik('Mladen', 'Moram još staviti razmak nakon imena')
-         p.innerHTML = polaznik8.ime + polaznik8.prezime;
+         let polaznik8 = new Polaznik('Mladen', 'Mladinić')
+         p.innerHTML = `${polaznik8.ime} ` + `${polaznik8.prezime}`;
          break;
       default:
          text = "Nešto nije dobro";
